9f96f5ef7b659fac8de66b0384a1e5e76c197962
Aytuğ Otmar <aytugotmar@outlook.com.tr>
2025-08-20 21:58:06 +0300

    first-publish

---
+++ WebApplication/WebApplication/Models/Model/ID.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication.Models.Model
+{
+    [Table("ID")]
+    public class ID
+    {
+        [Key]
+        public int IdentityId { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Keywords { get; set; }
+        [Required]
+        public string Description { get; set; }
+        public string LogoURL { get; set; }
+        [Required]
+        public string Slogan { get; set; }
+    }
+}
